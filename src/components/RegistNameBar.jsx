f94d5b1064fc3d7374ef26257a6de3c12f451a20
uchiyama-shintaro <uchiyama-shintaro@imjp.co.jp>
2018-09-01 18:20:23 +0900

    Mutation

--- src/components/RegistNameBar.jsx
+++ src/components/RegistNameBar.jsx
@@ -1,12 +1,46 @@
 import React from "react";
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
+
+const ADD_MEMBER = gql`
+mutation ReginstMember($name: String!){
+    registMember(name: $name) {
+      registMember {
+        id
+        name
+      }
+      errors {
+        code
+        message
+      }
+    }
+  }  
+`;
 
 function RegistNameBar({ ...props }) {
+    let input;
     return (
-        <form>
-            <div>
-                Name  <input type="text" />  <input type="submit" value="Regist" />
-            </div>
-        </form>
+        <Mutation mutation={ADD_MEMBER}>
+            {(addMember, { loading, error, data }) => (
+                <div>
+                    <form
+                        onSubmit={e => {
+                            e.preventDefault();
+                            addMember({ variables: { name: input.value } });
+
+                            input.value = "";
+                        }}
+                    >
+                        <div>
+                            Name  <input type="text" ref={node => { input = node; }} />  <input type="submit" value="Regist" />
+                        </div>
+                    </form>
+                    {data && <p>Regist Success!!</p>}
+                    {loading && <p>Loading...</p>}
+                    {error && <p>Error :( Please try again</p>}
+                </div>
+            )}
+        </Mutation>
     );
 }
 
--- src/views/RegistMember/RegistMember.jsx
+++ src/views/RegistMember/RegistMember.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import RegistNameBar from "../../components/RegistNameBar";
-import RegistNameResult from "../../components/RegistNameResult";
 
 function RegistMember({ ...props }) {
     return (
         <div>
             <p><b>RegistMember</b></p>
             <RegistNameBar />
-            <RegistNameResult />
         </div>
     );
 }
